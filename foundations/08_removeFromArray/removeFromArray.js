6fd496162f8c857369b97b79b519167b2e6cfe3b
peipp0815 <74204474+peipp0815@users.noreply.github.com>
2026-01-13 12:56:39 +0100

    Add foundations 08 solution

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...toRemove) {
+  for (let i = 0; i < toRemove.length; i++) {
+    const toRemoveArrayIndex = arr.indexOf(toRemove[i]);
+    if (toRemoveArrayIndex !== -1) {
+      arr.splice(toRemoveArrayIndex, 1);
+      i--;
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
