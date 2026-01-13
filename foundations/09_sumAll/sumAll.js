e25cb9580bb3fdb80bdcb5e2e18bb0f9574cdf6d
peipp0815 <74204474+peipp0815@users.noreply.github.com>
2026-01-13 14:32:51 +0100

    Add foundations 09 solution

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (sum1, sum2) {
+  if (Number.isInteger(sum1) && Number.isInteger(sum2)) {
+    if (sum1 < 0 || sum2 < 0) {
+      return "ERROR";
+    }
 
+    if (sum2 < sum1) {
+      const help = sum1;
+      sum1 = sum2;
+      sum2 = help;
+    }
+
+    let result = 0;
+    for (let i = sum1; i <= sum2; i++) {
+      result += i;
+    }
+
+    return result;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
