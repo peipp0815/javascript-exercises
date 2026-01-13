e4da51464c8e7dc024f4dcb3fc8e5c09c28faf30
peipp0815 <74204474+peipp0815@users.noreply.github.com>
2026-01-13 09:58:31 +0100

    Add foundations 06 solution

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  let repeatString = "";
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 1; i <= num; i++) {
+      repeatString += str;
+    }
+  }
+  return repeatString;
 };
 
 // Do not edit below this line
