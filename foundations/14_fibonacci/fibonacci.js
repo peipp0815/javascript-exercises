cc0674da7434d9a339b13623627c7e9bf2b30331
peipp0815 <74204474+peipp0815@users.noreply.github.com>
2026-02-06 20:17:21 +0100

    Add foundations 14 solution

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function (a) {
+  a = parseInt(a);
+  if (a < 0) {
+    return "OOPS";
+  }
+  let currentValue = 1;
+  let prevValue = 0;
+  let result = 1;
+  if (a === 0) {
+    result = 0;
+  }
+  while (a > 1) {
+    result = prevValue + currentValue;
+    prevValue = currentValue;
+    currentValue = result;
+    a--;
+  }
+  return result;
 };
 
 // Do not edit below this line
