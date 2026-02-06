d3fc34a34ed023e35e6d5940ec1568c517ff0440
peipp0815 <74204474+peipp0815@users.noreply.github.com>
2026-02-06 19:53:37 +0100

    Add foundations 13 solution

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const forward = string
+    .split("")
+    .filter((char) => {
+      return /[a-zA-Z0-9]/.test(char);
+    })
+    .join("")
+    .toLowerCase();
+  const backward = [...forward].reverse().join("");
+  return forward === backward;
 };
 
 // Do not edit below this line
