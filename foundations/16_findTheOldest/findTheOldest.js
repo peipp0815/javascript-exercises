5815bf76a31b1ae8ff6d6de355e45354e3d56df6
peipp0815 <74204474+peipp0815@users.noreply.github.com>
2026-02-06 21:55:44 +0100

    Add foundations 16 solution

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  return arr.toSorted((a, b) => {
+    let aDeath = a.yearOfDeath;
+    let bDeath = b.yearOfDeath;
+    if (aDeath === undefined) {
+      aDeath = new Date().getFullYear();
+    }
+    if (bDeath === undefined) {
+      bDeath = new Date().getFullYear();
+    }
+    return bDeath - b.yearOfBirth - (aDeath - a.yearOfBirth);
+  })[0];
 };
 
 // Do not edit below this line
