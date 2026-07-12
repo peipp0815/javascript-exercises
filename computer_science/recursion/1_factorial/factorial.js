85c6546fcc6a4866729157c7f8ab351e614bc845
peipp0815 <74204474+peipp0815@users.noreply.github.com>
2026-07-12 18:16:14 +0200

    Add recursion 01 solution

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,11 @@
-const factorial = function() {
-
+const factorial = function (n) {
+  if (Number.isInteger(n) === true) {
+    if (n === 0 || n === 1) {
+      return 1;
+    } else if (n > 1) {
+      return n * factorial(n - 1);
+    }
+  }
 };
 
 // Do not edit below this line
