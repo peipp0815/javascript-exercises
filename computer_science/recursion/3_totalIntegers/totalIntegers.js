ae4c75f4e00151a6e31530e4b08896aa04bbe7df
peipp0815 <74204474+peipp0815@users.noreply.github.com>
2026-07-19 13:05:24 +0200

    Add recursion 03 solution

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,20 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (array) {
+  if (typeof array === "object") {
+    let count = 0;
+    function findInts(value) {
+      if (typeof value === "object" && value !== null) {
+        Object.values(value).forEach((element) => {
+          findInts(element);
+        });
+      } else {
+        if (Number.isInteger(value) === true) {
+          count++;
+        }
+      }
+    }
+    findInts(array);
+    return count;
+  }
 };
-  
 // Do not edit below this line
 module.exports = totalIntegers;
