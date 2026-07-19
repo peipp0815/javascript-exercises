4dc461808f14dc99bbfdc567b56c2c52649a6f0b
peipp0815 <74204474+peipp0815@users.noreply.github.com>
2026-07-19 11:47:15 +0200

    Add recursion 02 solution

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,12 @@
-const contains = function() {
-  
+const contains = function (obj, str) {
+  const val = Object.values(obj);
+  if (val.includes(str)) return true;
+  const nestedObjects = val.filter(
+    (value) => typeof value === "object" && value !== null,
+  );
+
+  return nestedObjects.some((nestedObject) => contains(nestedObject, str));
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
